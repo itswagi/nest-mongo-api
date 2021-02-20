989f100c4b6d28d766106073e0c0e3aa83152b65
itswagi <43064115+itswagi@users.noreply.github.com>
2021-02-20 19:21:17 +0500

    Created resources using nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BoardsModule } from './boards/boards.module';
+import { CandidatesModule } from './candidates/candidates.module';
+import { ListsModule } from './lists/lists.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE),
-    // CatsModule,
+    BoardsModule,
+    CandidatesModule,
+    ListsModule,
+    MembersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
