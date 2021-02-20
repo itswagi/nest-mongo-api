8aa2652bc946b3fa7b939814cd126ab4c02816ea
itswagi <43064115+itswagi@users.noreply.github.com>
2021-02-20 18:03:32 +0500

    Completed example mongo db setup

---
+++ src/schemas/cat.schema.ts
@@ -0,0 +1,18 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type CatDocument = Cat & Document;
+
+@Schema()
+export class Cat {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  breed: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
